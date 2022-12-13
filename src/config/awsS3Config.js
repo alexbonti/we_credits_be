006e67c8e2c5f2568bd4f50c7b845ae9be3d80fb
Jason Pham <61170796+jasonhtpham@users.noreply.github.com>
2022-12-13 15:46:53 +1100

    update keys

--- src/config/awsS3Config.js
+++ src/config/awsS3Config.js
@@ -1,13 +1,15 @@
+import 'dotenv/config';
 /*
 *Please add project folder name and ibm bucket name here,
 * make sure project folder name doesnt not have spaces in between and is same
 * as the name you give while running upload_setup.sh
 */
+
 var s3BucketCredentials = {
     "projectFolder": "wecredits-bucket",
     "bucket": "wecredits-bucket",
-    "accessKeyId": "AKIA46K4LMSSEKBVQSRX",
-    "secretAccessKey": "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",
+    "accessKeyId": process.env.AWS_ACCESS_KEY,
+    "secretAccessKey": process.env.AWS_SECRET_KEY,
     "s3URL": "https://wecredits-bucket.s3.ap-southeast-2.amazonaws.com",
     "folder": {
         "profilePicture": "profilePicture",
